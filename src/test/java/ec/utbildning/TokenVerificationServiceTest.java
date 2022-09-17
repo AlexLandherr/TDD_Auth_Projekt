96af698fc4e24c49374e4c4579a4b30ff05a6f0e
Alex Landherr <alex.landherr@gmail.com>
2022-09-17 15:01:51 +0200

    Completed Red & Green phase for 'IncorrectToken' test of TokenVerificationService.

--- src/test/java/ec/utbildning/TokenVerificationServiceTest.java
+++ src/test/java/ec/utbildning/TokenVerificationServiceTest.java
@@ -53,4 +53,24 @@ public class TokenVerificationServiceTest {
         //Then
         Assertions.assertEquals(expectedOutcome, result);
     }
+
+    //anna
+    //berit
+    //kalle
+    @ParameterizedTest
+    @CsvSource(value = {                                  //↓ arrow points to where the tokens were changed.
+            "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbm5hIiwiUGFzc3avcmQiOiJsb3NlbiJ9.ohJE-JmNnDiDVBEkWwXh_5vft8-fyiA8lGPRrzid5VlhUDG3nTCpzv9rTGh7wBmPBlqOnwCh9-O05Mz--P1IGQ, false",
+            "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiZXJpdCIsIlBhc3b3b3JkIjoiMTIzNDU2In0.0QU8FQJDiTnh7AXALKfNI9HEzol2HGCCjCBaTlW4ToIm9c6Z9DY9icmNhLzgbV10wf5B87RB4EPHjqcXowcfBQ, false",
+            "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrYWxsZSIsIlBhc3c3b3JkIjoicGFzc3dvcmQifQ.5dWlb3zHE13RLXREtT2tLM4d40jpQpRFkEzn7NLB5qbKlFLPVFEf3LPZoD7I4qg5xdfTp3IkM-oTIfQY1Vbsug, false"
+    })
+    public void IncorrectToken(String token, boolean expectedOutcome) {
+        //Given
+        when(userRepo.findAll()).thenReturn(userList);
+
+        //When
+        boolean result = tokenVerificationService.tokenCheck(token);
+
+        //Then
+        Assertions.assertEquals(expectedOutcome, result);
+    }
 }
